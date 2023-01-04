a530bb29e1614023f5dc444732d9f7638ee5be86
TSOlami <tijani.saheed@yahoo.com>
2023-01-04 23:52:51 +0100

    commit all

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Tijani Saheed
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Tijani Saheed
+
+def pow(a, b):
+    return (a ** b)
