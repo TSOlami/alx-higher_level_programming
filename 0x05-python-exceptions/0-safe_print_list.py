2165a571beef5d25063e2f7b56e255947aabda7e
TSOlami <tijani.saheed@yahoo.com>
2023-01-23 12:34:16 -0800

    Exceptions

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+	count = 0
+	for num in range(x):
+		try:
+			print("{}".format(my_list[num]), end="")
+			count = count + 1
+		except (ValueError, TypeError, IndexError):
+			break 
+	print()
+	return count
