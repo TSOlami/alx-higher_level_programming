604bf464d0f3bc16ff3323a66d301928f1afc6f1
TSOlami <tijani.saheed@yahoo.com>
2023-01-24 09:06:13 -0800

    Correct all errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
-	count = 0
-	for num in range(x):
-		try:
-			print("{}".format(my_list[num]), end="")
-			count = count + 1
-		except (ValueError, TypeError, IndexError):
-			break 
-	print()
-	return count
+    total = 0
+    for i in range(x):
+        try:
+            print(f"{my_list[i]}", end="")
+            total += 1
+        except IndexError:
+            break
+    print()
+    return(total)
--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-	try:
-		result = a / b
-		return (result)
-	except:
-		result = "None"
-	finally:
-		print("Inside result: {}".format(result))
+    """Returns the division of a by b."""
+    try:
+        div = a / b
+    except (TypeError, ZeroDivisionError):
+        div = None
+    finally:
+        print("Inside result: {}".format(div))
+    return (div))
