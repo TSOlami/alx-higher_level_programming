d92f773e778518a5bd703faf785f2578292078fd
TSOlami <tijani.saheed@yahoo.com>
2023-05-23 09:39:47 -0700

    Fixing Task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const movieId = process.argv[2];
 const request = require('request');
-const url = 'https://swapi-api.alx-tools.com/api/https://swapi-api.alx-tools.com/api/https://swapi-api.alx-tools.com/api/films/' + movieId;
+const url = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   } else {
-    const jsondata = JSON.parse(body);
-    console.log(jsondata.title);
+    const jsonData = JSON.parse(body);
+    console.log(jsonData.title);
   }
 });
