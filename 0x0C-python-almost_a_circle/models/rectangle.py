2c5c82b73eabedc25f35bb294642f42c2ee70a14
TSOlami <tijani.saheed@yahoo.com>
2023-06-12 12:42:53 +0100

    0x0C-python-almost_a_circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -2,7 +2,7 @@
 """
     Class Rectangle
 """
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,7 +2,7 @@
 """
     contains class Square implements class Rectangle
 """
-from models.rectangle import Rectangle
+from rectangle import Rectangle
 
 
 class Square(Rectangle):
